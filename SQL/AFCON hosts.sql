0a03dcf03a95971d79cff8eb4ca90063ce123292
rafidbilly <156634263+rafidbilly@users.noreply.github.com>
2024-01-25 14:13:16 +0700

    Update AFCON hosts.sql

--- SQL/AFCON hosts.sql
+++ SQL/AFCON hosts.sql
@@ -1,19 +1,7 @@
-SELECT DISTINCT
-    YEAR(matchdate)
-FROM
-    african_nations_results_clean
-WHERE
-    tournament = 'african cup of nations';
-    
 CREATE TABLE afcon_host (
 	hostyear INT,
     host_team VARCHAR(255)
 );
-
-ALTER TABLE afcon_host
-DROP COLUMN host_team_2;
-
-SELECT * FROM afcon_host;
 
 INSERT INTO afcon_host(hostyear, host_team)
 VALUES
